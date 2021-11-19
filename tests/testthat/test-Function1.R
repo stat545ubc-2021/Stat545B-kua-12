870b3d54708b2b378ebc01a748d2ec9ccb8b0e0a
Andrea Ku <andreaku12@gmail.com>
2021-11-18 22:18:29 -0800

    Updated according to feedback

--- tests/testthat/test-Function1.R
+++ tests/testthat/test-Function1.R
@@ -2,7 +2,9 @@ Testing <- test_that("Testing function", {
   FlowFunc <- Function1(datateachr::flow_sample,month,flow)
   expect_true(is.data.frame(datateachr::flow_sample))
   expect_true(is.numeric(datateachr::flow_sample$flow))
-  expect_error(Function_test(datateachr::flow_sample,month))
-  expect_error(Function_test(datateachr::flow_sample,flow))
+  expect_error(Function1(datateachr::flow_sample,month))
+  expect_error(Function1(datateachr::flow_sample,flow))
+  expect_s3_class(Function1(flow_sample,month,flow), c("tbl_df", "tbl", "data.frame", "list"))
+  }
+  )
 
-})
